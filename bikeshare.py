88c6864db9e5a69a5472b422c272818d61891190
Akinbande-Tolulope <akinbandetolulope@gmail.com>
2021-12-17 12:32:23 +0100

    Update Bikeshare.py
    Reword the comment for getting user inputs under get_filters

--- bikeshare.py
+++ bikeshare.py
@@ -7,7 +7,7 @@ from datetime import timedelta as td
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-              
+
 
 def get_filters():
     """
@@ -19,14 +19,14 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington). While loop used to handle invalid input
     while True:
         city = str.lower(input('Which city would you like to get data from? Chicago, New York City or Washington?: '))
         if city.lower() not in ('chicago', 'washington', 'new york city'):
             print('Sorry, that is not a valid city. Try again!')
         else:
             break
-       
+
     # get user input for month (all, january, february, ... , june)
     while True:
         month = str.lower(input('Should the data be for all months or a specific month(January to June)?  '))
@@ -57,7 +57,7 @@ def load_data(city, month, day):
     Returns:
         df - pandas DataFrame containing city data filtered by month and day
     """
-    
+
     # load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
 
@@ -77,17 +77,17 @@ def load_data(city, month, day):
         # use the index of the months list to get the corresponding int
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) + 1
-    
+
         # filter by month to create the new dataframe
         df = df[df['month'] == month]
 
     # filter by day of week if applicable
     if day != 'all':
-        
-        
+
+
         # filter by day of week to create the new dataframe
         df = df[df['day_of_week'] == day.title()]
-    
+
     return df
 
 
@@ -153,11 +153,11 @@ def user_stats(df):
     print('\nCalculating User Stats...\n')
     start_time = time.time()
 
-    # Display counts of user types    
+    # Display counts of user types
     print('The categories of users and the count of each are as follow\n', df['User Type'].value_counts())
 
     # Display counts of gender
-    """Washington does not have gender and birth year data. 
+    """Washington does not have gender and birth year data.
     Using try to avoid break of code"""
     try:
         print('\nThe number of users for each gender are\n', df['Gender'].value_counts())
@@ -182,7 +182,7 @@ def user_stats(df):
         # Display this for city(s) without birth year data
         print('Birth year data is unavailable for this city')
 
-        
+
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
